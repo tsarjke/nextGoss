c71e3036b8665b16f1ddc36d8a57c8cd0e7c9446
Ivan <ivantsaryok@yandex.ru>
2022-09-20 17:55:05 +0300

    correct exercise

--- pages/sample/index.js
+++ pages/sample/index.js
@@ -4,7 +4,7 @@ export const getServerSideProps = async ({ res }) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
 	res.setHeader('X-Author', 'tsarjke');
-  res.end('const task = (x) => x*this**2;');
+  res.end('function task(x) { return x*this**2;}');
   return { props: {} };
 }
 
