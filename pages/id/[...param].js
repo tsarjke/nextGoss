0adad60d367af6c5f0427844bbe5b39539dc381d
Ivan <ivantsaryok@yandex.ru>
2022-10-03 17:02:43 +0600

    add req by id

---
+++ pages/id/[...param].js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const plain = () => null;
+
+export const getServerSideProps = async ({ res, req }) => {
+	res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+	res.setHeader('X-Author', 'tsarjke');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	const requrl = req.url.split('/');
+	const id = requrl[2];
+	if (id && !isNaN(id)) {
+		const response = await axios.get(`https://nd.kodaktor.ru/users/${id}`);
+		const { data } = response;
+		res.end(data?.login);
+	}
+  return { props: {} };
+}
+
+export default plain;
